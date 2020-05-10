c64c1da29801d2b0749e7ec80c460195d66fdaef
Diago Estrada <estradadiago93@gmail.com>
2020-05-10 18:02:05 -0400

    Added routing

--- src/views/SignUp/SignUp.js
+++ src/views/SignUp/SignUp.js
@@ -6,15 +6,17 @@ import {makeStyles} from '@material-ui/styles';
 import {
     Grid,
     Button,
-    IconButton,
     TextField,
     Link,
     FormHelperText,
     Checkbox,
-    Typography,
-    MenuItem
+    Typography
 } from '@material-ui/core';
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import {Auth} from 'aws-amplify';
+import Alert from "@material-ui/lab/Alert/Alert";
+import InputAdornment from "@material-ui/core/InputAdornment/InputAdornment";
+import IconButton from "@material-ui/core/IconButton/IconButton";
+import {Visibility, VisibilityOff} from "@material-ui/icons";
 
 const schema = {
     firstName: {
@@ -37,9 +39,18 @@ const schema = {
         }
     },
     password: {
-        presence: {allowEmpty: false, message: 'is required'},
+        presence: {
+            allowEmpty: false,
+            message: 'is required'
+        },
         length: {
-            maximum: 128
+            minimum: 8,
+            maximum: 128,
+            message: "must be at least 8 characters"
+        },
+        format: {
+            pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,128}$/,
+            message: "must contain at least one numeric digit, one uppercase and one lowercase letters"
         }
     },
     policy: {
@@ -52,11 +63,21 @@ const schema = {
             maximum: 32
         }
     },
+    phoneNumber: {
+        presence: {allowEmpty: false, message: 'is required'},
+        length: {
+            maximum: 32
+        }
+        // format: {
+        //     pattern: /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/,
+        //     message: 'in not valid'
+        // }
+    }
 };
 
 const useStyles = makeStyles(theme => ({
     root: {
-        backgroundColor: theme.palette.background.default,
+        backgroundColor: '#3f51b5',
         height: '100%'
     },
     grid: {
@@ -114,22 +135,24 @@ const useStyles = makeStyles(theme => ({
         flexGrow: 1,
         display: 'flex',
         alignItems: 'center',
-        [theme.breakpoints.down('md')]: {
-            justifyContent: 'center'
-        }
+        justifyContent: 'center'
     },
     form: {
-        paddingLeft: 100,
-        paddingRight: 100,
-        paddingBottom: 125,
-        flexBasis: 700,
+        paddingLeft: 25,
+        paddingRight: 25,
+        paddingBottom: 25,
+        paddingTop: 25,
+        flexBasis: 500,
+        border: '1px solid',
+        borderRadius: '5px',
+        backgroundColor: theme.palette.background.default,
         [theme.breakpoints.down('sm')]: {
             paddingLeft: theme.spacing(2),
             paddingRight: theme.spacing(2)
         }
     },
     title: {
-        marginTop: theme.spacing(3)
+        // marginTop: theme.spacing(3)
     },
     textField: {
         marginTop: theme.spacing(2)
@@ -144,20 +167,30 @@ const useStyles = makeStyles(theme => ({
     },
     signUpButton: {
         margin: theme.spacing(2, 0)
+    },
+    resendCode: {
+        justifyContent: 'flex-end',
+        alignItems: 'center'
     }
 }));
 
 const SignUp = props => {
+    const url = new URLSearchParams(props.location.search);
     const {history} = props;
-
     const classes = useStyles();
-
+    const [code, setCode] = useState('');
+    const [signedUp, setSignedUp] = useState(url.get('signup') || false);
     const [formState, setFormState] = useState({
         isValid: false,
-        values: {},
+        values: {
+            'email': url.get('username') || ''
+        },
         touched: {},
         errors: {}
     });
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [message, setMessage] = useState(null);
+    const [showPassword, setShowPassword] = useState(false);
 
     useEffect(() => {
         const errors = validate(formState.values, schema);
@@ -188,14 +221,61 @@ const SignUp = props => {
         }));
     };
 
-    const handleBack = () => {
-        history.goBack();
+    const handleChangeCode = event => {
+        event.persist();
+        setCode(event.target.value)
     };
 
-    const handleSignUp = event => {
+    const signUp = async event => {
         event.preventDefault();
-        history.push('/');
+        const {password, email, firstName, lastName, phoneNumber} = formState.values;
+
+        try {
+            await Auth.signUp({
+                username: email,
+                password: password,
+                attributes: {
+                    email: email,
+                    given_name: firstName,
+                    family_name: lastName,
+                    phone_number: phoneNumber,
+                    updated_at: Date.now().toString()
+                }
+            });
+            setSignedUp(true);
+        } catch (e) {
+            setErrorMessage(e.message);
+            setMessage(null);
+        }
     };
+
+    const confirmSingUp = async event => {
+        event.preventDefault();
+        const {email, organization} = formState.values;
+
+        try {
+            await Auth.confirmSignUp(email, code, {clientMetadata: {['organization_name']: organization}});
+            history.push('/sign-in');
+            setMessage(null);
+            setErrorMessage(null);
+        } catch (e) {
+            setErrorMessage(e.message)
+        }
+    }
+
+    const resendCode = async event => {
+        event.preventDefault();
+        const {email} = formState.values;
+
+        try {
+            await Auth.resendSignUp(email);
+            setMessage("Code has been sent");
+            setErrorMessage(null);
+        }catch (e) {
+            setMessage(null);
+            setErrorMessage(e.message);
+        }
+    }
 
     const hasError = field =>
         formState.touched[field] && formState.errors[field] ? true : false;
@@ -206,53 +286,16 @@ const SignUp = props => {
                 className={classes.grid}
                 container
             >
-                <Grid
-                    className={classes.quoteContainer}
-                    item
-                    lg={5}
-                >
-                    <div className={classes.quote}>
-                        <div className={classes.quoteInner}>
-                            <Typography
-                                className={classes.quoteText}
-                                variant="h1"
-                            >
-                                Hella narwhal Cosby sweater McSweeney's, salvia kitsch before
-                                they sold out High Life.
-                            </Typography>
-                            <div className={classes.person}>
-                                <Typography
-                                    className={classes.name}
-                                    variant="body1"
-                                >
-                                    Takamaru Ayako
-                                </Typography>
-                                <Typography
-                                    className={classes.bio}
-                                    variant="body2"
-                                >
-                                    Manager at inVision
-                                </Typography>
-                            </div>
-                        </div>
-                    </div>
-                </Grid>
-                <Grid
+                {!signedUp && <Grid
                     className={classes.content}
                     item
-                    lg={7}
+                    lg={12}
                     xs={12}
                 >
                     <div className={classes.content}>
-                        <div className={classes.contentHeader}>
-                            <IconButton onClick={handleBack}>
-                                <ArrowBackIcon/>
-                            </IconButton>
-                        </div>
                         <div className={classes.contentBody}>
                             <form
                                 className={classes.form}
-                                onSubmit={handleSignUp}
                             >
                                 <Typography
                                     className={classes.title}
@@ -266,6 +309,10 @@ const SignUp = props => {
                                 >
                                     Use your email to create new account
                                 </Typography>
+                                {errorMessage &&
+                                <Alert className={classes.errorMessage} variant="outlined" severity="error">
+                                    {errorMessage}
+                                </Alert>}
                                 <TextField
                                     className={classes.textField}
                                     error={hasError('firstName')}
@@ -295,23 +342,19 @@ const SignUp = props => {
                                     variant="outlined"
                                 />
                                 <TextField
-                                    select
                                     className={classes.textField}
-                                    error={hasError('organization')}
+                                    error={hasError('phoneNumber')}
                                     fullWidth
                                     helperText={
-                                        hasError('organization') ? formState.errors.organization[0] : null
+                                        hasError('phoneNumber') ? formState.errors.phoneNumber[0] : null
                                     }
-                                    label="Organization"
-                                    name="organization"
+                                    label="Phone Number"
+                                    name="phoneNumber"
                                     onChange={handleChange}
                                     type="text"
-                                    value={formState.values.organization || ''}
+                                    value={formState.values.phoneNumber || ''}
                                     variant="outlined"
                                 >
-                                    <MenuItem value={"Organization_1"}>Organization 1</MenuItem>
-                                    <MenuItem value={"Organization_2"}>Organization 2</MenuItem>
-                                    <MenuItem value={"Organization_3"}>Organization 3</MenuItem>
                                 </TextField>
                                 <TextField
                                     className={classes.textField}
@@ -337,10 +380,38 @@ const SignUp = props => {
                                     label="Password"
                                     name="password"
                                     onChange={handleChange}
-                                    type="password"
+                                    type={showPassword ? "text" : "password"}
                                     value={formState.values.password || ''}
                                     variant="outlined"
+                                    InputProps={{
+                                        endAdornment: (
+                                            <InputAdornment position="end">
+                                                <IconButton
+                                                    aria-label="toggle password visibility"
+                                                    onClick={() => setShowPassword(!showPassword)}
+                                                    edge="end"
+                                                >
+                                                    {!showPassword ? <Visibility/> : <VisibilityOff/>}
+                                                </IconButton>
+                                            </InputAdornment>
+                                        )
+                                    }}
                                 />
+                                <TextField
+                                    className={classes.textField}
+                                    error={hasError('organization')}
+                                    fullWidth
+                                    helperText={
+                                        hasError('organization') ? formState.errors.organization[0] : null
+                                    }
+                                    label="Organization"
+                                    name="organization"
+                                    onChange={handleChange}
+                                    type="text"
+                                    value={formState.values.organization || ''}
+                                    variant="outlined"
+                                >
+                                </TextField>
                                 <div className={classes.policy}>
                                     <Checkbox
                                         checked={formState.values.policy || false}
@@ -379,6 +450,7 @@ const SignUp = props => {
                                     size="large"
                                     type="submit"
                                     variant="contained"
+                                    onClick={signUp}
                                 >
                                     Sign up now
                                 </Button>
@@ -398,7 +470,90 @@ const SignUp = props => {
                             </form>
                         </div>
                     </div>
-                </Grid>
+                </Grid>}
+                {signedUp && <Grid
+                    className={classes.content}
+                    item
+                    lg={12}
+                    xs={12}
+                >
+                    <div className={classes.content}>
+                        <div className={classes.contentBody}>
+                            <form
+                                className={classes.form}
+                            >
+                                <Typography
+                                    className={classes.title}
+                                    variant="h2"
+                                >
+                                    Verification account
+                                </Typography>
+                                <Typography
+                                    color="textSecondary"
+                                    gutterBottom
+                                >
+                                    Code has been sent to {formState.values.email}
+                                </Typography>
+                                {errorMessage &&
+                                <Alert className={classes.errorMessage} variant="outlined" severity="error">
+                                    {errorMessage}
+                                </Alert>}
+                                {message &&
+                                <Alert className={classes.errorMessage} variant="outlined" severity="success">
+                                    {message}
+                                </Alert>}
+                                <TextField
+                                    className={classes.textField}
+                                    fullWidth
+                                    label="Confirmation Code"
+                                    name="code"
+                                    onChange={handleChangeCode}
+                                    type="code"
+                                    value={code || ''}
+                                    variant="outlined"
+                                />
+                                <Button
+                                    className={classes.signUpButton}
+                                    color="primary"
+                                    disabled={!code}
+                                    fullWidth
+                                    size="large"
+                                    type="submit"
+                                    variant="contained"
+                                    onClick={confirmSingUp}
+                                >
+                                    Send code
+                                </Button>
+                                <Grid container spacing={1}>
+                                    <Grid container item xs={6} justify="space-between">
+                                        <Typography
+                                            color="textSecondary"
+                                            variant="body1"
+                                        >
+                                            Have an account?{' '}
+                                            <Link
+                                                component={RouterLink}
+                                                to="/sign-in"
+                                                variant="h6"
+                                            >
+                                                Sign in
+                                            </Link>
+                                        </Typography>
+                                    </Grid>
+                                    <Grid container item xs={6} spacing={3} className={classes.resendCode}>
+                                        <Link
+                                            component="button"
+                                            variant="h6"
+                                            onClick={resendCode}
+                                        >
+                                            Resend Code
+                                        </Link>
+                                    </Grid>
+                                </Grid>
+                            </form>
+                        </div>
+                    </div>
+                </Grid>}
             </Grid>
         </div>
     );
