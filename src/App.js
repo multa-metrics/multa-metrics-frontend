9b21669b2475e7a47e527df8f59881989a9c303c
Diago Estrada <estradadiago93@gmail.com>
2020-06-15 22:30:36 -0400

    Added identityPoolId to config

--- src/App.js
+++ src/App.js
@@ -20,8 +20,10 @@ Amplify.configure(
     {
         "aws_cognito_region": process.env.REACT_APP_AWS_COGNITO_REGION,
         "aws_user_pools_id": process.env.REACT_APP_AWS_USER_POOLS_ID,
-        "aws_user_pools_web_client_id": process.env.REACT_APP_AWS_USER_POOLS_WEB_CLIENT_ID
-    });
+        "aws_user_pools_web_client_id": process.env.REACT_APP_AWS_USER_POOLS_WEB_CLIENT_ID,
+        "identity_pool_id": process.env.REACT_APP_AWS_IDENTITY_POOL_ID
+    }
+);
 const browserHistory = createBrowserHistory();
 
 Chart.helpers.extend(Chart.elements.Rectangle.prototype, {
